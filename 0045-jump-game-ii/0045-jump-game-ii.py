f63e114ced17c3fa8dfb7877b8d9f84bfc650940
Aham0803 <ahamgolash9340@gmail.com>
2026-08-07 09:30:10 +0530

    Time: 4 ms (70.13%), Space: 20 MB (65.21%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jump = 0
+        currend = 0
+        far = 0
+        for i in range(len(nums) -1 ):
+            far = max(far , i + nums[i])
+
+            if(i == currend):
+                jump += 1
+                currend = far
+        
+        return jump
+
+        
