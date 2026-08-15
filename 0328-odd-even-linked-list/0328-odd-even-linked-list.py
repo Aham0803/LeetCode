609059086e741550b7e7f4f6e5fbf6454d536ff7
Aham0803 <ahamgolash9340@gmail.com>
2026-08-15 19:45:23 +0530

    Time: 0 ms (100%), Space: 21.1 MB (36.85%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -6,29 +6,46 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         # brute force
-        if head == None or head.next == None:
+        # if head == None or head.next == None:
+        #     return head        
+        # values =[]
+        # temp = head
+        # while temp != None:
+        #     values.append(temp.val)
+        #     if temp.next == None:
+        #         break
+        #     temp = temp.next.next
+        # temp = head.next
+        # while temp != None:
+        #     values.append(temp.val)
+        #     if temp.next is None:
+        #         break
+        #     temp =  temp.next.next
+        # temp = head
+        # index = 0
+        # while(temp is not None):
+        #     temp.val = values[index]
+        #     index += 1
+        #     temp = temp.next
+        # return head
+
+        # optimal
+        if head is None or head.next is None:
             return head
         
-        values =[]
-        temp = head
-        while temp != None:
-            values.append(temp.val)
-            if temp.next == None:
-                break
-            temp = temp.next.next
-            
-        temp = head.next
-        while temp != None:
-            values.append(temp.val)
-            if temp.next is None:
-                break
-            temp =  temp.next.next
+        odd = head
+        even = head.next
+        even_head = even
+        while even is not None and even.next is not None:
+            odd.next = odd.next.next
+            odd = odd.next
+            even.next = even.next.next
+            even = even.next
         
-        temp = head
-        index = 0
-        while(temp is not None):
-            temp.val = values[index]
-            index += 1
-            temp = temp.next
+        odd.next = even_head
         return head
+            
+        
+
+        
         
