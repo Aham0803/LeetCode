9fc114ef6eecb3af6aaa7114fe362ca24c1be4b0
Aham0803 <ahamgolash9340@gmail.com>
2026-08-16 18:25:03 +0530

    Time: 1 ms (62.63%), Space: 27.7 MB (97.18%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,21 +8,49 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        arr = []
-        temp = head
+        #brute force
+        # arr = []
+        # temp = head
+        # while(temp):
+        #     arr.append(temp)
+        #     temp = temp.next
+        # left = 0
+        # right = len(arr)-1
+        # while(left < right):
+        #     arr[left].next = arr[right]
+        #     left += 1
+        #     arr[right].next = arr[left]
+        #     right -= 1
+        # arr[left].next = None
 
-        while(temp):
-            arr.append(temp)
-            temp = temp.next
-        
-        left = 0
-        right = len(arr)-1
+        # optimal
+        slow = head
+        fast = head
 
-        while(left < right):
-            arr[left].next = arr[right]
-            left += 1
+        #finding middle
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        # reverse second half
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
 
-            arr[right].next = arr[left]
-            right -= 1
+        second = prev
 
-        arr[left].next = None
+        # merging
+        first = head
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
