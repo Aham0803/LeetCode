3202260ec68fa5c86821f4ea0d0a81ad11731396
Aham0803 <ahamgolash9340@gmail.com>
2026-08-24 19:26:53 +0530

    Time: 119 ms (16.43%), Space: 35.1 MB (13.2%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,60 @@
+# class Solution:
+#     def rotate(self, nums: list[int], k: int) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         k = k%len(nums)
+#         left = 0
+#         right = len(nums)-k
+
+#         for i in range(k):
+#             nums[left] , nums[right]= nums[right] , nums[left]
+#             left += 1
+#             right += 1
+        
+#         if len(nums) %2 != 0:
+#             nums[k:] = sorted(nums[k:])
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k = k % len(nums)
+
+        # reverse whole array
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+        # reverse first k
+        left = 0
+        right = k - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+        # reverse remaining
+        left = k
+        right = len(nums) - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
