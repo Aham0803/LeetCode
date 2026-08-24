60175f7af2bec5482ac566e9a21396acca4075b4
Aham0803 <ahamgolash9340@gmail.com>
2026-08-24 19:32:41 +0530

    Time: 19 ms (26.08%), Space: 35.3 MB (7.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -16,34 +16,40 @@
 #             nums[k:] = sorted(nums[k:])
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        # k = k % len(nums)
+
+        # # reverse whole array
+        # left = 0
+        # right = len(nums) - 1
+
+        # while left < right:
+        #     nums[left], nums[right] = nums[right], nums[left]
+        #     left += 1
+        #     right -= 1
+
+        # # reverse first k
+        # left = 0
+        # right = k - 1
+
+        # while left < right:
+        #     nums[left], nums[right] = nums[right], nums[left]
+        #     left += 1
+        #     right -= 1
+
+        # # reverse remaining
+        # left = k
+        # right = len(nums) - 1
+
+        # while left < right:
+        #     nums[left], nums[right] = nums[right], nums[left]
+        #     left += 1
+        #     right -= 1
+
+        # by slicing
+
         k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
 
-        # reverse whole array
-        left = 0
-        right = len(nums) - 1
-
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
-
-        # reverse first k
-        left = 0
-        right = k - 1
-
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
-
-        # reverse remaining
-        left = k
-        right = len(nums) - 1
-
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
 
 
 
