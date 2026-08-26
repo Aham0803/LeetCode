ccaf0b9ea2d5c74c1d820f058d9bdf31357dbbe2
Aham0803 <ahamgolash9340@gmail.com>
2026-08-26 10:34:59 +0530

    Time: 4 ms (89.34%), Space: 19.6 MB (52.33%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,23 +1,36 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        # if n == 0:
+        #     return True
+        # for i in range(len(flowerbed)):
+        #     if flowerbed[i] == 0:
+        #         if i == 0:
+        #             left = 0
+        #         else:
+        #             left = flowerbed[i-1]
+        #         if i == len(flowerbed) -1:
+        #             right = 0
+        #         else :
+        #             right = flowerbed[i+1]
+        #         if left == 0 and right == 0:
+        #             flowerbed[i] = 1
+        #             n -= 1
+        #             if n == 0:
+        #                 return True
+        # return n == 0
+
         if n == 0:
             return True
-        for i in range(len(flowerbed)):
-            if flowerbed[i] == 0:
-                if i == 0:
-                    left = 0
-                else:
-                    left = flowerbed[i-1]
-            
-                if i == len(flowerbed) -1:
-                    right = 0
-                else :
-                    right = flowerbed[i+1]
-            
-                if left == 0 and right == 0:
-                    flowerbed[i] = 1
-                    n -= 1
-            
-                    if n == 0:
-                        return True
+
+        flowerbed = [0] + flowerbed + [0]
+
+        for i in range(1 , len(flowerbed)-1):
+            if flowerbed[i-1] == 0 and flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+
+                if n == 0:
+                    return True
+        
         return n == 0
+        
