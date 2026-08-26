e2a1f5b47cddd5bb3ca2020af0f0d91ffde8c908
Aham0803 <ahamgolash9340@gmail.com>
2026-08-26 14:17:59 +0530

    Time: 12 ms (64.13%), Space: 20.5 MB (37.32%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -3,16 +3,17 @@ class Solution:
         n = len(arr)
         ans = 0
         for i in range(1,n-1):
+            #  i ko peak maan
             if arr[i-1] < arr[i] and arr[i] > arr[i+1]:
                 left = i
                 right = i
-
+                # left side decreasing he check kr
                 while left > 0 and arr[left] > arr[left-1]:
                     left -= 1
+                # right side dec h check kr
                 while right < n-1 and arr[right] > arr[right+1]:
                     right += 1
                 
-                length = right-left +1
-                ans = max(ans , length)
+                ans = max(ans , right-left+1)
     
         return ans
