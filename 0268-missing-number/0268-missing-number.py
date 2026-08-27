fc15b75cf9551a2b30554ae5bba8693c8f47c45e
Aham0803 <ahamgolash9340@gmail.com>
2026-08-27 20:09:42 +0530

    Time: 0 ms (100%), Space: 20.4 MB (41.21%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        for i in range(len(nums)+1):
-            if i not in nums:
-                return i
+        # for i in range(len(nums)+1):
+        #     if i not in nums:
+        #         return i
+
+        # optimal best
+        n = len(nums)
+        ans = n
+
+        for i in range(n):
+            ans = ans ^ i ^ nums[i]
+
+        return ans
