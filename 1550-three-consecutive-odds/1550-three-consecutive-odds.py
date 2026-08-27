38380291da4bb90177af69f867804b3c5a37ce3e
Aham0803 <ahamgolash9340@gmail.com>
2026-08-27 20:15:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.43%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.py
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -6,10 +6,9 @@ class Solution:
                 count += 1
                 if count == 3:
                     return True
-                    break
+                    
             else:
                 count = 0
-        
         return False
         
         
