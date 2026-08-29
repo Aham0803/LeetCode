7f0f62f045f9bcb9ea4ba61e2cc2b85c2d8f9c96
Aham0803 <ahamgolash9340@gmail.com>
2026-08-29 19:35:40 +0530

    Time: 12 ms (40.44%), Space: 19.4 MB (14.68%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.py
@@ -6,12 +6,15 @@ class Solution:
         # else:
         #     return nums[1]
 
-        minV = nums[0]
-        maxV = nums[0]
+        # minV = nums[0]
+        # maxV = nums[0]
 
-        for v in nums:
-            minV= min(minV , v)
-            maxV = max(maxV , v)
+        # for v in nums:
+        #     minV= min(minV , v)
+        #     maxV = max(maxV , v)
+        
+        minV = min(nums)
+        maxV = max(nums)
         
         for v in nums:
             if v != minV and v != maxV:
