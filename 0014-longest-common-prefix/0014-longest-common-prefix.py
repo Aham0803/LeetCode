63d9161f5b2dd3dba0a24a7f89160a65a9213f3c
Aham0803 <ahamgolash9340@gmail.com>
2026-08-29 20:49:20 +0530

    Time: 0 ms (100%), Space: 19.4 MB (7.07%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -10,15 +10,31 @@ class Solution:
         #     ans = ans[:j] 
         # return ans
 
-        if len(strs) == 0:
-            return ""
-        result = ""
-        base = strs[0]
-        for i in range(len(base)):
-            for word in strs[1:]:
-                if i == len(word) or word[i] != base[i]:
-                    return result
+        # if len(strs) == 0:
+        #     return ""
+        # result = ""
+        # base = strs[0]
+        # for i in range(len(base)):
+        #     for word in strs[1:]:
+        #         if i == len(word) or word[i] != base[i]:
+        #             return result
         
-            result += base[i]
-        return result
+        #     result += base[i]
+        # return result
         
+        N = len(strs[0])
+        length = 0
+        for i in range(0,N):
+            check = strs[0][i]
+            flag = True
+            for v in strs:
+                if len(v) <= i or v[i] != check:
+                    flag = False
+                    break
+            
+            if flag:
+                length += 1
+            else:
+                break
+        
+        return strs[0][0:length]
