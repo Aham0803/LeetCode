9856254fdecdf40da98f47b83e0b06120c1cd30b
Aham0803 <ahamgolash9340@gmail.com>
2026-09-01 00:15:40 +0530

    Time: 60 ms (39.5%), Space: 29.6 MB (38.21%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -3,15 +3,17 @@ class Solution:
         ans = 0
         n = len(height)
         l = 0
-        r= n-1 
-        while(l < r):
+        r = n-1
+
+        while (l < r):
             h = min(height[l] , height[r])
-            wt =  r- l
-            area = h * (r-l)
+            width = r-l
+            area = h*width
             ans = max(area , ans)
-            
-            if(height[l] < height[r]):
+
+            if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
+        
         return ans
