d65c520df1232862731de672af3bd98f74d0f526
Aham0803 <ahamgolash9340@gmail.com>
2026-08-31 13:40:25 +0530

    Time: 692 ms (77.44%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,16 +1,32 @@
-SELECT
-   d.name as Department,
-   e.name as Employee,
-   e.salary as Salary
-from Employee e
-join Department as d
-on e.departmentId = d.id
-join(
+-- SELECT
+--    d.name as Department,
+--    e.name as Employee,
+--    e.salary as Salary
+-- from Employee e
+-- join Department as d
+-- on e.departmentId = d.id
+-- join(
+--     select
+--         departmentId,
+--         max(salary) as max_salary
+--     from Employee
+--     group by departmentId
+-- )m
+-- on e.departmentId = m.departmentId
+-- And e.salary = m.max_salary;
+
+select
+Department,
+Employee,
+Salary
+from(
     select
-        departmentId,
-        max(salary) as max_salary
-    from Employee
-    group by departmentId
-)m
-on e.departmentId = m.departmentId
-And e.salary = m.max_salary;
+    d.name as Department,
+    e.name as Employee,
+    e.salary as Salary,
+    dense_rank() over(partition by e.departmentId order by e.salary desc) as rnk
+    from Employee e
+    join Department d
+        on e.departmentID = d.id
+)t
+where rnk = 1;
