14df6a81268694c28a49b99f7d510c09378b9cf1
Aham0803 <ahamgolash9340@gmail.com>
2026-08-31 19:12:23 +0530

    Time: 0 ms (100%), Space: 19.7 MB (16.26%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -5,10 +5,15 @@ class Solution:
         for i in range(0,N):
             sumD += mat[i][i]
         
+        colLn = N-1
+        # for i in range(0,N):
+        #     sumD += mat[i][N-i-1]
+
         for i in range(0,N):
-            sumD += mat[i][N-i-1]
-        
+            sumD += mat[i][colLn]
+            colLn -=1
         if N%2 != 0:
             mid = N//2
             sumD -= mat[mid][mid]
         return sumD
+        
