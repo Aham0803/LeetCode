6b096ba9dd406265366957a0ba72b9d2ae87c673
Aham0803 <ahamgolash9340@gmail.com>
2026-09-04 20:55:37 +0530

    Time: 23 ms (5.17%), Space: 19.3 MB (71.43%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -1,11 +1,32 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
+        # ans = -1
+        # n = len(nums)
+        # for i in range(0,n):
+        #     maxV = max(nums[0:i+1])
+        #     minV = min(nums[i:])
+        #     dif = maxV-minV
+        #     if dif <= k:
+        #         ans = i
+        #         break
+        # return ans
+
         ans = -1
-        n = len(nums)
-        for i in range(0,n):
-            maxV = max(nums[0:i+1])
-            minV = min(nums[i:])
-            dif = maxV-minV
+        N = len(nums)
+        def findMax(start, end):
+            val = nums[start]
+            for i in range(start, end+1):
+                val = max(nums[i], val)
+            return val
+        def findMin(start, end):
+            val = nums[start]
+            for i in range(start, end+1):
+                val = min(nums[i], val)
+            return val
+        for i in range(0, N):
+            maxV = findMax(0,i)
+            minV = findMin(i, N-1)
+            dif = maxV - minV
             if dif <= k:
                 ans = i
                 break
