f6e2643359e1ac6eb5d7309b37d56fb30a2cfda7
Aham0803 <ahamgolash9340@gmail.com>
2026-09-04 20:40:34 +0530

    Time: 0 ms (100%), Space: 19.3 MB (22%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.py
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.py
@@ -1,11 +1,29 @@
 class Solution:
     def countOppositeParity(self, nums: list[int]) -> list[int]:
+        # ans = []
+        # N = len(nums)
+        # for i in range(0,N):
+        #     count = 0
+        #     for j in range(i+1 , N):
+        #         if nums[i] % 2 != nums[j]%2:
+        #             count += 1
+        #     ans.append(count)
+        # return ans
+
+        # three computatiion use
+        odd , even = 0 , 0
+        for v in nums:
+            if v%2 == 0:
+                even += 1
+            else:
+                odd += 1
         ans = []
-        N = len(nums)
-        for i in range(0,N):
-            count = 0
-            for j in range(i+1 , N):
-                if nums[i] % 2 != nums[j]%2:
-                    count += 1
-            ans.append(count)
+        for v in nums:
+            if v%2 != 0:
+                ans.append(even)
+                odd -= 1
+            else:
+                ans.append(odd)
+                even -= 1
         return ans
+
