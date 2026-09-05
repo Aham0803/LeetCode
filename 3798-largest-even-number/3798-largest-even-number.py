64a036938d91967ca55016efae623633a8c4ca35
Aham0803 <ahamgolash9340@gmail.com>
2026-09-05 20:10:24 +0530

    Time: 0 ms (100%), Space: 19.4 MB (17.68%) - LeetHub

--- 3798-largest-even-number/3798-largest-even-number.py
+++ 3798-largest-even-number/3798-largest-even-number.py
@@ -1,7 +1,17 @@
 class Solution:
     def largestEven(self, s: str) -> str:
-        for i in range(len(s)-1 , -1 , -1):
-            if int(s[i]) % 2 == 0:
-                return s[:i+1]
-        return ""
+        # for i in range(len(s)-1 , -1 , -1):
+        #     if int(s[i]) % 2 == 0:
+        #         return s[:i+1]
+        # return ""
+
+        index  = -1
+        n = len(s)
+        for i in range(n-1 , -1,-1):
+            val = int(s[i])
+            if val % 2 == 0:
+                index = i
+                break
+        return s[:index+1]
+
         
