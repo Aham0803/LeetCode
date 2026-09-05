3842740047a1756c67f83af190bea63eaa802167
Aham0803 <ahamgolash9340@gmail.com>
2026-09-05 19:38:52 +0530

    Time: 20 ms (6.47%), Space: 19.2 MB (89.12%) - LeetHub

---
+++ 3833-count-dominant-indices/3833-count-dominant-indices.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dominantIndices(self, nums: List[int]) -> int:
+        ans = 0
+        n = len(nums)
+        for i in range(len(nums)-1):
+            sum , count = 0 , 0
+            for j in range(i+1 , n):
+                sum += nums[j]
+                count += 1
+            avg = sum / count
+            if avg < nums[i]:
+                ans +=1
+        return ans
