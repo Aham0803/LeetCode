32926058d7d2951c3c2a1b8e3bed039237cfeb49
Aham0803 <ahamgolash9340@gmail.com>
2026-09-17 12:03:36 +0530

    Time: 861 ms (27.74%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,44 @@
+-- # Write your MySQL query statement below
+-- SELECT
+--     s.product_id,
+--     s.year AS first_year,
+--     s.quantity,
+--     s.price
+-- FROM Sales s
+-- JOIN (
+--     SELECT
+--         product_id,
+--         MIN(year) AS first_year
+--     FROM Sales
+--     GROUP BY product_id
+-- ) t
+-- ON s.product_id = t.product_id
+-- AND s.year = t.first_year;
+
+select
+product_id, 
+year as first_year,
+quantity,
+price
+from Sales s
+where (product_id , year) in(
+    select
+    product_id,
+    min(year)
+    from Sales
+    group by product_id
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
