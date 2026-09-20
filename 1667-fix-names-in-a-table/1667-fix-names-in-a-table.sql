5c30356ea6c890f3f70adfc73750aaa75a52324c
Aham0803 <ahamgolash9340@gmail.com>
2026-09-20 18:13:25 +0530

    Time: 704 ms (64.48%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
 select
 user_id,
 concat(
@@ -7,3 +7,31 @@ concat(
 ) as name
 from Users
 order by user_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
