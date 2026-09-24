c84a017743fe311ea9b85ed27ae4029e01c59e95
Aham0803 <ahamgolash9340@gmail.com>
2026-09-24 20:29:19 +0530

    Time: 13 ms (49.54%), Space: 21.7 MB (97.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -10,10 +10,30 @@ class Solution:
         #     freq[key].append(word)
         # return list(freq.values())
 
-        freq = {}
+        # freq = {}
+        # for word in strs:
+        #     key = ''.join(sorted(word))
+        #     if key not in freq:
+        #         freq[key] = []
+        #     freq[key].append(word)
+        # return list(freq.values())
+
+        d={}
         for word in strs:
-            key = ''.join(sorted(word))
-            if key not in freq:
-                freq[key] = []
-            freq[key].append(word)
-        return list(freq.values())
+            key=''.join(sorted(word))
+            if key in d:
+                d[key].append(word)
+            else:
+                d[key]=[word]
+        return list(d.values())
+
+
+
+
+
+
+
+
+
+
+
